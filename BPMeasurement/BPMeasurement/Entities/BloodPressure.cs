b78a1b6aae12981bef3e41b971ec7a16706c6917
lgswin <lgswin@gmail.com>
2024-02-10 21:46:12 -0500

    error handling

--- BPMeasurement/BPMeasurement/Entities/BloodPressure.cs
+++ BPMeasurement/BPMeasurement/Entities/BloodPressure.cs
@@ -6,8 +6,14 @@ namespace BPMeasurement.Entities
 	public class BloodPressure
 	{
 		public int BloodPressureId { get; set; }
-		public int Systolic { get; set; }
-		public int Diastolic { get; set; }
+
+        [Required(ErrorMessage = "Please enter a correct number.")]
+        [Range(10, 200, ErrorMessage = "Blood pressure must be between 10 and 200.")]
+        public int Systolic { get; set; }
+
+        [Required(ErrorMessage = "Please enter a correct number.")]
+        [Range(10, 200, ErrorMessage = "Blood pressure must be between 10 and 200.")]
+        public int Diastolic { get; set; }
 
 		[DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
@@ -15,8 +21,7 @@ namespace BPMeasurement.Entities
 
         [Required(ErrorMessage ="Please select a position")]
 		public string? PositionId { get; set; }
-
-		public Position? Position { get; set; }
+        public Position? Position { get; set; }
 
         public string Category
         {
